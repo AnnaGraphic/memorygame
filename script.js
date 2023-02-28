8208799cdfb5dc97b53467d4753c5bd0a75f49a0
panda <p4nd4@posteo.de>
2023-02-28 08:56:21 +0100

    check for matches

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const cards = document.querySelectorAll('.memory-card');
+
+let hasCardFlipped = false;
+let firstCard, secondCard;
+
 function flipCard() {
     // console.log("clicked!");
-    this.classList.toggle('flip')
+    this.classList.add('flip');
+
+    if (!hasCardFlipped) {
+        // 1. click
+        hasCardFlipped = true;
+        firstCard = this;
+        //  console.log("click 1", hasCardFlipped, firstCard)
+    } else {
+      hasCardFlipped = false;
+      secondCard = this;
+      // find match
+      if (firstCard.dataset.name === secondCard.dataset.name) {
+        // a match
+        firstCard.removeEventListener('click', flipCard);
+        firstCard.removeEventListener('click', flipCard);
+      } else {
+        //not a match
+        setTimeout(() => {
+          firstCard.classList.remove('flip');
+          secondCard.classList.remove('flip');
+        }, 1600)
+      }
+    }
 }
+
 cards.forEach(card => card.addEventListener("click", flipCard))
